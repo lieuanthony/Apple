efc6fe2495cc15b84bfea27e6e5f3bd9c5f79644
Anthony Lieu <al6907@g.rit.edu>
2024-03-04 23:37:07 -0500

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Anthony  Lieu on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
