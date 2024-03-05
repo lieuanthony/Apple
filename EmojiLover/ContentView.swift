ffe6f10d41d8b990826beeffa6addb75db80c21c
Anthony Lieu <al6907@g.rit.edu>
2024-03-05 00:20:35 -0500

    added a view of the emoji and a slider to change the emoji

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😭, 😅, 😥, 🤯
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😅
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)}
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
